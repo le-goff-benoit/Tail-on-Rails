29b09ca1b2a3ca45ebc35f6007ecf777de2b8353
Benoît Le Goff <benoit.le.goff@icloud.com>
2020-12-26 17:53:51 +0100

    Finish tailwind config file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
+    extend: {
+      colors: {
+        primary: {
+          100: "#fef6eb",
+          200: "#f7c686",
+          300: "#f4b35d",
+          400: "#f2aa49",
+          500: "#f1a035",
+          600: "#d99030",
+          700: "#c1802a",
+          800: "#a97025",
+          900: "#916020",
+        },
+      },
+    },
   variants: {
     extend: {},
   },
